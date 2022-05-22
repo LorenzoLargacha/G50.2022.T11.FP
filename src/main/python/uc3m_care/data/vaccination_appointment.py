61c4d87c60f0690cbbd6c46069a234b0c0f498e6
SonsolesMolinaAbad <100432073@alumnos.uc3m.es>
2022-05-23 00:40:13 +0200

    Refactorización: tipado VaccinationAppointment

--- src/main/python/uc3m_care/data/vaccination_appointment.py
+++ src/main/python/uc3m_care/data/vaccination_appointment.py
@@ -21,8 +21,7 @@ from uc3m_care.parser.cancelation_json_parser import CancelationJsonParser
 #pylint: disable=too-many-instance-attributes
 class VaccinationAppointment():
     """Class representing an appointment  for the vaccination of a patient"""
-
-    def __init__(self, patient_sys_id, patient_phone_number, date,appointment_status="Active"):
+    def __init__(self, patient_sys_id: str, patient_phone_number: str, date: str, appointment_status="Active") -> None:
         self.__alg = "SHA-256"
         self.__type = "DS"
         self.__patient_sys_id = PatientSystemId(patient_sys_id).value
@@ -37,76 +36,75 @@ class VaccinationAppointment():
         self.__appointment_status = AppointmentStatus(appointment_status).value
 
 
-    def __signature_string(self):
+    def __signature_string(self) -> str:
         """Composes the string to be used for generating the key for the date"""
         return "{alg:" + self.__alg +",typ:" + self.__type +",patient_sys_id:" + \
                self.__patient_sys_id + ",issuedate:" + self.__issued_at.__str__() + \
                ",vaccinationtiondate:" + self.__appointment_date.__str__() + "}"
 
     @property
-    def patient_id(self):
+    def patient_id(self) -> str:
         """Property that represents the guid of the patient"""
         return self.__patient_id
 
     @patient_id.setter
-    def patient_id(self, value):
+    def patient_id(self, value) -> None:
         self.__patient_id = value
 
     @property
-    def patient_sys_id(self):
+    def patient_sys_id(self) -> str:
         """Property that represents the patient_sys_id of the patient"""
         return self.__patient_sys_id
 
     @patient_sys_id.setter
-    def patient_sys_id(self, value):
+    def patient_sys_id(self, value) -> None:
         self.__patient_sys_id = value
 
     @property
-    def phone_number(self):
+    def phone_number(self) -> str:
         """Property that represents the phone number of the patient"""
         return self.__phone_number
 
     @phone_number.setter
-    def phone_number(self, value):
+    def phone_number(self, value: str) -> None:
         self.__phone_number = PhoneNumber(value).value
 
     @property
-    def vaccination_signature(self):
+    def vaccination_signature(self) -> str:
         """Returns the sha256 signature of the date"""
         return hashlib.sha256(self.__signature_string().encode()).hexdigest()
 
     @property
-    def issued_at(self):
+    def issued_at(self) -> float:
         """Returns the issued at value"""
         return self.__issued_at
 
     @issued_at.setter
-    def issued_at(self, value):
+    def issued_at(self, value: float) -> None:
         self.__issued_at = value
 
     @property
-    def appointment_date( self ):
+    def appointment_date(self) -> float:
         """Returns the vaccination date"""
         return self.__appointment_date
 
     @property
-    def date_signature(self):
+    def date_signature(self) -> str:
         """Returns the SHA256 """
         return self.__date_signature
 
     @property
-    def appointment_status(self):
+    def appointment_status(self) -> str:
         """Property that represents the appointment_status of an appointment"""
         return self.__appointment_status
 
-
-    def save_appointment(self):
+    def save_appointment(self) -> None:
         """saves the appointment in the appointments store"""
         appointments_store = AppointmentsJsonStore()
         appointments_store.add_item(self)
 
     @classmethod
-    def get_appointment_from_date_signature(cls, date_signature):
+    def get_appointment_from_date_signature(cls, date_signature: str):
         """returns the vaccination appointment object for the date_signature received"""
         appointments_store = AppointmentsJsonStore()
         appointment_record = appointments_store.find_item(DateSignature(date_signature).value)
@@ -124,7 +122,7 @@ class VaccinationAppointment():
         return appointment
 
     @classmethod
-    def create_appointment_from_json_file(cls, json_file, date):
+    def create_appointment_from_json_file(cls, json_file: str, date: str):
         """returns the vaccination appointment for the received input json file"""
         appointment_parser = AppointmentJsonParser(json_file)
         new_appointment = cls(
@@ -133,7 +131,7 @@ class VaccinationAppointment():
             date)
         return new_appointment
 
-    def is_valid_today(self):
+    def is_valid_today(self) -> True:
         """returns true if today is the appointment's date"""
         today = datetime.today().date()
         date_patient = datetime.fromtimestamp(self.appointment_date).date()
@@ -141,7 +139,7 @@ class VaccinationAppointment():
             raise VaccineManagementException("Today is not the date")
         return True
 
-    def register_vaccination(self):
+    def register_vaccination(self) -> True:
         """register the vaccine administration"""
         if self.is_valid_today():
             #Comprobamos que la cita no haya sido cancelada
@@ -152,7 +150,7 @@ class VaccinationAppointment():
         return True
 
     @classmethod
-    def modify_appointment_from_json_file(cls, input_file):
+    def modify_appointment_from_json_file(cls, input_file: str):
         """returns the vaccination appointment object for the input_file received
         and modifies the appointment_status"""
         # Intentamos abrir el fichero de entrada, validamos las keys y guardamos sus datos
@@ -171,7 +169,7 @@ class VaccinationAppointment():
         appointment.modify_appointment_status(cancelation_type)
         return appointment
 
-    def modify_appointment_status(self, cancelation_type):
+    def modify_appointment_status(self, cancelation_type: str) -> None:
         """Modifica el status de la cita en el objeto VaccinationAppoinment"""
         # Comprobamos si la fecha de la cita recibida ya ha pasado
         appointment_days = (self.__appointment_date / 3600) / 24
@@ -204,7 +202,7 @@ class VaccinationAppointment():
         elif self.__appointment_status == "Cancelled Final":
             raise VaccineManagementException("Cita ya cancelada de forma Final")
 
-    def modify_store_date(self):
+    def modify_store_date(self) -> None:
         """Modifica el status de la cita en store_date"""
         appointments_store = AppointmentsJsonStore()
         appointments_store.update_item(self, self.__date_signature)

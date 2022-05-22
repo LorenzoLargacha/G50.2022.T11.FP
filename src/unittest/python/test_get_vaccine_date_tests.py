b3b2447cbd7addfc9335ef6df20858fb35d9a568
SonsolesMolinaAbad <100432073@alumnos.uc3m.es>
2022-05-22 22:33:30 +0200

    Refactorización: eliminamos test_get_vaccine_date_no_ok y lo metemos en param_list_nok para evitar código duplicado

--- src/unittest/python/test_get_vaccine_date_tests.py
+++ src/unittest/python/test_get_vaccine_date_tests.py
@@ -48,7 +48,8 @@ param_list_nok = [("test_dup_all.json", "2022-03-18", "JSON Decode Error - Wrong
                   ("test_ok.json", "2022-03-32", "wrong date format"),
                   ("test_ok.json", "0000-03-18", "wrong date format"),
                   ("test_ok.json", "2022-03-07", "date should be after today"),
-                  ("test_nok_no_comillas.json", "2022-03-18", "JSON Decode Error - Wrong JSON Format")
+                  ("test_nok_no_comillas.json", "2022-03-18", "JSON Decode Error - Wrong JSON Format"),
+                  ("test_no_ok.json", "2022-03-18", "patient system id is not valid")
                   ]
 
 
@@ -104,28 +105,6 @@ class TestGetVaccineDate(TestCase):
                 hash_new = file_store_date.data_hash()
 
                 self.assertEqual(hash_new, hash_original)
-
-
-    @freeze_time("2022-03-08")
-    def test_get_vaccine_date_no_ok(self):
-        """# long 32 in patient system id , not valid"""
-        file_test = JSON_FILES_RF2_PATH + "test_no_ok.json"
-        date = "2022-03-18"
-        my_manager = VaccineManager()
-        file_store_date = AppointmentsJsonStore()
-
-        # read the file to compare file content before and after method call
-        hash_original = file_store_date.data_hash()
-
-        #check the method
-        with self.assertRaises(VaccineManagementException) as c_m:
-            my_manager.get_vaccine_date(file_test, date)
-        self.assertEqual(c_m.exception.message, "patient system id is not valid")
-
-        # read the file again to campare
-        hash_new = file_store_date.data_hash()
-
-        self.assertEqual(hash_new, hash_original)
 
     @freeze_time("2022-03-08")
     def test_get_vaccine_date_no_ok_data_manipulated( self ):
